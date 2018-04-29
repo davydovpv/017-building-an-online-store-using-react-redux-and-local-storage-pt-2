ec916bacc04714cd3487a99e313af8c90f03ee49
Mark Tellez <mark.tellez@icloud.com>
2018-04-29 13:19:57 -0500

    cart quantity now stored

--- src/features/product-listing/product-list-item.js
+++ src/features/product-listing/product-list-item.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function ProductListItem(props) {
-  const thisItemInCart = props.cart.filter(item => item.id === props.product.id)[0]
   return <div className='product-list-item'>
     <h3>{ props.product.name }</h3>
     <img
@@ -15,7 +14,7 @@ export default function ProductListItem(props) {
       <button
         onClick={() => props.addToCart(props.product)}
       >Add to cart ({
-        (thisItemInCart && thisItemInCart.quantity) || 0
+        (props.cartItem && props.cartItem.quantity) || 0
       })</button>
     </div>
   </div>
